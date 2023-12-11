5fe3f2b8114b8fb8e69cb07029eb96ea6e7215d2
TdzZone <135322334+TdzZone@users.noreply.github.com>
2023-12-11 13:38:59 +0100

    Update database.js

--- models/database.js
+++ models/database.js
@@ -2,11 +2,11 @@ const mysql2 = require('mysql2/promise')
 
 let pool = mysql2.createPool({
     connectionLimit: 10000,
-    host: "localhost",// on rentre l'hôte, l'adresse url où se trouve la bdd
-    port: 3001 ,//  port sur le quel est la bddd
-    user: "root", // identifiant BDD
-    password: "", // le password
-    database: "", // nom de la base de donnée
+    host: "localhost",
+    port: 3001 ,
+    user: "root", 
+    password: "", 
+    database: "", 
 });
 
 module.exports = pool
